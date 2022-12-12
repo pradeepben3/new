60b49b6f21731d46c7f8884cc8271b023ed24f41
pradeepben3 <97969637+pradeepben3@users.noreply.github.com>
2022-12-12 17:02:57 +0530

    Update cuda.cu

--- cuda.cu
+++ cuda.cu
@@ -278,11 +278,11 @@ cleanup:
     return chunks_per_gpu;
 }
 
-int main() {
+uint64_t main() {
     struct timeval tv1, tv2;
     gettimeofday(&tv1, NULL);
     sieve(1e9);
     gettimeofday(&tv2, NULL);
     printf("Total time = %f seconds\n", (double) (tv2.tv_usec - tv1.tv_usec) / 1000000 + (double) (tv2.tv_sec - tv1.tv_sec));
-    return 22;
+    return sieve(1e8);
 } 
