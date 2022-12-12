da43f40fc1fe74955dbcc6dca33f24bee932019e
pradeepben3 <97969637+pradeepben3@users.noreply.github.com>
2022-12-12 16:48:28 +0530

    Update cuda.cu

--- cuda.cu
+++ cuda.cu
@@ -283,4 +283,5 @@ int main() {
     sieve(1e9);
     gettimeofday(&tv2, NULL);
     printf("Total time = %f seconds\n", (double) (tv2.tv_usec - tv1.tv_usec) / 1000000 + (double) (tv2.tv_sec - tv1.tv_sec));
-}
+    return 22;
+} 
